52efbf73755e3447bb4d8124cf4005c8610d6ad7
DanielGMorenaFhecor <dgm@fhecor.es>
2023-03-23 13:14:06 +0100

    pyenv installer

--- installer.ps1
+++ installer.ps1
@@ -1,3 +1,8 @@
-Import-Module -Name .\src\settings_reader.ps1 -Verbose
+Import-Module -Name .\src\settings_reader.ps1
+Import-Module -Name .\src\pyenv_installer.ps1
 
-$settings = Read-Settings -SettingsPath 'settings.txt'
+# Read settings
+$settings = Read-Settings
+
+# Install python version if it does not exist using pyenv
+Install-PyEnv -Version $settings['PythonVersion']
---
+++ src/pyenv_installer.ps1
@@ -0,0 +1,61 @@
+
+function Install-PyEnv {
+    Param(
+        [string]$Version
+    )
+
+    # Check if pyenv exists
+    if (!Test-PyEnvExists) {
+        DownloadAndInstallContent
+    }
+
+    # Check if specified python version is installed
+    if (!IsPythonVersion-Installed) {
+        Install-PythonVersion
+    }
+}
+
+function Test-PyEnvExists {
+    $oldPreference = $ErrorActionPreference
+    try {
+        if (Get-Command "pyenv") {
+            return $true
+        }
+    }
+    Catch {
+        return $false
+    }
+    Finally {
+        $ErrorActionPreference = $oldPreference
+    }
+}
+
+function IsPythonVersion-Installed {
+    Param(
+        [string]$Version
+    )
+    $versions = pyenv versions
+    return $versions.Contains($Version)
+}
+
+function DownloadAndInstallContent {
+
+    # Download
+    Write-Output "Installing Pyenv..."
+    $githubUrl = "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1"
+    Invoke-WebRequest -UseBasicParsing -Uri $githubUrl -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+
+    #Updates the PATH variable in the current session
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User") 
+}
+
+function Install-PythonVersion {
+    Param(
+        [string]$Version
+    )
+    Write-Output "Installing Python version $Version..."
+    pyenv install $Version
+    Write-Output "Python $Version Installed!"
+}
+
+Install-PythonVersion -Version "3.9.13" | Write-Output
